c0848717406c37c94ed8b73ccbc5ebe9f45cb878
Vincent Buzzano <vincent.buzzano@gmail.com>
2021-06-25 01:23:55 +0000

    clean script

--- test/custom-token.js
+++ test/custom-token.js
@@ -23,15 +23,10 @@ describe('Week 5 - Task 1 & 2', () => {
             issuer: 3.1 * wvs,
             bob: 0.03 * wvs,
         })
-
         const script = compile(file('vault.ride'))
         const ssTx = setScript({ script }, accounts.dApp)
         const tx = await broadcast(ssTx)
         await waitForTx(tx.id)
-
-        console.log(`dApp account ${address(accounts.dApp)}`)
-        console.log(`Issuer account ${address(accounts.issuer)}`)
-        console.log(`Bob account ${address(accounts.bob)}`)
     })
 
     it('Issue usual token', async () => {
@@ -51,7 +46,7 @@ describe('Week 5 - Task 1 & 2', () => {
 
         assetId = tx.id
 
-        console.log(`Token created : ${tx.id}`)
+        // console.log(`Token created : ${tx.id}`)
     })
 
     it('Re-issue 100 more tokens', async () => {
@@ -65,7 +60,7 @@ describe('Week 5 - Task 1 & 2', () => {
         const tx = await broadcast(stx)
         await waitForTx(tx.id)
 
-        console.log(`Token re-issued successfully : ${tx.id}`)
+        // console.log(`Token re-issued successfully : ${tx.id}`)
     })
 
     it('Transfer 50 tokens to any address', async () => {
@@ -78,7 +73,7 @@ describe('Week 5 - Task 1 & 2', () => {
         const tx = await broadcast(ttx)
         await waitForTx(tx.id)
 
-        console.log(`Transfer successful : ${tx.id}`)
+        // console.log(`Transfer successful : ${tx.id}`)
     })
 
     it('Update asset script to allow only InvokeScript transactions with the token and deny all others', async () => {
@@ -91,22 +86,17 @@ describe('Week 5 - Task 1 & 2', () => {
         const tx = await broadcast(stx)
         await waitForTx(tx.id)
 
-        console.log(`Token script updated successfully : ${tx.id}`)
+        // console.log(`Token script updated successfully : ${tx.id}`)
     })
 
     it('Re Transfer 50 tokens to any address Should failed', async () => {
-        try {
-            const ttx = transfer({
-                amount: 50,
-                assetId,
-                fee: 0.005 * wvs,
-                recipient: address(accounts.bob),
-            }, accounts.issuer)
-            const tx = await broadcast(ttx)
-            await waitForTx(tx.id)
-        } catch (error) {
-            console.log(`Transaction failed: ${error.message}`)
-        }
+        const ttx = transfer({
+            amount: 50,
+            assetId,
+            fee: 0.005 * wvs,
+            recipient: address(accounts.bob),
+        }, accounts.issuer)
+        await expect(broadcast(ttx)).rejectedWith()
     })
 
     it('Wallet Deposit ', async () => {
@@ -120,11 +110,9 @@ describe('Week 5 - Task 1 & 2', () => {
             }],
             fee: 900000,
         }
-        console.log('AAAAAAA')
         const stx = invokeScript(params, accounts.bob)
-        console.log(stx)
         const tx = await broadcast(stx)
         await waitForTx(tx.id)
-        console.log(`Deposit done -> ${tx.id}`)
+        // console.log(`Deposit done -> ${tx.id}`)
     })
 })
